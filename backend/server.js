f31afe9a0b55394b15d4b116247b8b9927331829
Sanchali12 <girisoma164@gmail.com>
2026-08-13 15:56:01 +0530

    Fix port

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ const crypto = require("crypto");
 
 const transporter = nodemailer.createTransport({
   host: "smtp-relay.brevo.com",
-  port:587,
+  port:2525,
   secure: false,
   auth: {
     user: process.env.EMAIL_USER,
