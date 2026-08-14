231e71622968ea2cff614ad779dd9b8ee563e6c7
Sanchali12 <girisoma164@gmail.com>
2026-08-15 01:41:07 +0530

    do changes

--- backend/server.js
+++ backend/server.js
@@ -234,7 +234,43 @@ app.post("/signup", async (req, res) => {
 
     
 
+app.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
 
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({
+        message: "User not found"
+      });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(400).json({
+        message: "Invalid password"
+      });
+    }
+
+    const token = jwt.sign(
+      { id: user._id },
+      "mysecretkey",
+      { expiresIn: "7d" }
+    );
+
+    res.json({
+      message: "Login successful",
+      token
+    });
+
+  } catch (err) {
+    res.status(500).json({
+      message: err.message
+    });
+  }
+});
 app.post("/google-login", async (req, res) => {
   try {
     const { credential } = req.body;
